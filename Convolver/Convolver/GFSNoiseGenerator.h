4519d18159c28488a5924ab26a32f70436f82d3a
Bill Dudney <bdudney@mac.com>
2012-04-09 07:04:16 -0600

    dded the firt cut at the noise generator, now it's just random noise using randome to fill each pixel, eventually I'd like to make it 2D Perlin noise

---
+++ Convolver/Convolver/GFSNoiseGenerator.h
@@ -0,0 +1,13 @@
+//
+//  GFSNoiseGenerator.h
+//  Convolver
+//
+//  Created by Bill Dudney on 4/6/12.
+//  Copyright (c) 2012 Gala Factory Software, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GFSNoiseGenerator : NSObject
+
+@end
---
+++ Convolver/Convolver/GFSNoiseGenerator.m
@@ -0,0 +1,13 @@
+//
+//  GFSNoiseGenerator.m
+//  Convolver
+//
+//  Created by Bill Dudney on 4/6/12.
+//  Copyright (c) 2012 Gala Factory Software, LLC. All rights reserved.
+//
+
+#import "GFSNoiseGenerator.h"
+
+@implementation GFSNoiseGenerator
+
+@end
